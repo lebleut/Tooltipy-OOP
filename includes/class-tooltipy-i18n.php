a64bb428e359d54ac844026240b69d42a3fa4de7
Jamel Eddine Zarga <jamel.zarga@gmail.com>
2018-07-21 19:07:16 +0100

    Fix Tooltipy textdomain

--- includes/class-tooltipy-i18n.php
+++ includes/class-tooltipy-i18n.php
@@ -35,7 +35,7 @@ class Tooltipy_i18n {
 	public function load_plugin_textdomain() {
 
 		load_plugin_textdomain(
-			'tooltipy',
+			'tooltipy-lang',
 			false,
 			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
 		);

c84cbc7c2312f7839b05d26077c9c6d4b8e595bd
Jamel Eddine Zarga <jamel.zarga@gmail.com>
2018-07-22 17:18:14 +0100

    Add npm ftp-deploy workflow to allow upload all files all selected ones to the site to speed up development wprkflow
    Note create a .env file in which to add the environment variables needed as ftp cridentials

---
+++ npm_scripts/deploy-some.js
@@ -0,0 +1,52 @@
+var FtpDeploy = require('ftp-deploy');
+var ftpDeploy = new FtpDeploy();
+const gitFiles = require('git-files')
+var dotenv = require('dotenv').config();
+
+var fs = require('fs');
+
+var myfiles = JSON.parse(fs.readFileSync('./npm_scripts/upload_files.json', 'utf8'));
+upload_files = []
+myfiles.forEach( file => {
+    if( file.upload ){
+        upload_files.push(file.path.trim())
+    }
+});
+
+var config = {
+    host: process.env.SITE_HOST, // Create a .env file in which you store the environment vars
+    port: 21,
+    user: process.env.SITE_USER,
+    password: process.env.SITE_PASS,
+
+    localRoot: __dirname+'/..',
+    remoteRoot: process.env.SITE_REMOTE_ROOT,
+    //include: [ '*', 'includes/**', 'css/**', 'js/**' ],      // this would upload everything except dot files
+    include: upload_files,
+    exclude: [
+        'package.json',
+        'node_modules/**/*',
+        'npm_scripts/**/*',
+        '.git/**/*',
+        'dist/**/*.map',
+    ],     // e.g. exclude sourcemaps
+    deleteRoot: true                // delete existing files at destination before uploading
+}
+
+ftpDeploy.deploy(config)
+    .then(res => console.log('**** UPLOAD DONE ****'))
+    .catch(err => console.log(err))
+
+ftpDeploy.on('uploading', function(data) {
+
+});
+
+ftpDeploy.on('uploaded', function(data) {
+    console.log(data.filename+' ... Uploaded')
+});
+
+ftpDeploy.on('upload-error', function (data) {
+    console.log('****** upload - ERROR ******');
+    console.log(data.err); // data will also include filename, relativePath, and other goodies
+    console.log('------------------------------');
+}); 
